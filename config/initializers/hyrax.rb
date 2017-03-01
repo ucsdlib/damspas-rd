44be2471a53361a0d2713b94acb7fbf478f9b926
VivianChu <tchu@ucsd.edu>
2017-03-01 09:54:25 -0800

    Initial setup and configuration for GA Integration
    Use environment variable to enable displaying usage statistics in the UI
    
    Fix syntax error

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -16,13 +16,13 @@ Hyrax.config do |config|
   # Enable displaying usage statistics in the UI
   # Defaults to FALSE
   # Requires a Google Analytics id and OAuth2 keyfile.  See README for more info
-  # config.analytics = false
+  config.analytics = ENV.fetch('APPS_HORTON_ANALYTICS'){'false'}
 
   # Specify a Google Analytics tracking ID to gather usage statistics
-  # config.google_analytics_id = 'UA-99999999-1'
+  config.google_analytics_id = ENV.fetch('APPS_HORTON_GOOGLE_ID'){'UA-99999999-1'}
 
   # Specify a date you wish to start collecting Google Analytic statistics for.
-  # config.analytic_start_date = DateTime.new(2014,9,10)
+  config.analytic_start_date = DateTime.new(2014,9,10)
 
   # Enables a link to the citations page for a generic_file.
   # Default is false
